ed7f8be238f95115dc4aa319b010c10d31082e1a
RevathyGnanam <revathygovindasamy241193@gmail.com>
2026-07-14 07:48:04 +0530

    Added new file

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -3,12 +3,10 @@ import { TestConfig } from '../test.config'
 import { HomePage } from '../pages/HomePage';
 import { LoginPage } from '../pages/LoginPage';
 import { MyAccountPage } from '../pages/MyAccountPage';
-
 let config: TestConfig;
 let accpage: MyAccountPage;
 let homepage: HomePage;
 let loginPage: LoginPage;
-
 test.beforeEach(async ({ page }) => {
     config = new TestConfig();
     await page.goto(config.appUrl); //navigation to the app url
